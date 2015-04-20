5f6f00e534b278f3d04b757391ab9f439654a9bd
Bryce Anderson <keeputahweird@gmail.com>
2015-04-19 21:01:56 -0600

    v0.0.13

--- test/test_fun_with_files.rb
+++ test/test_fun_with_files.rb
@@ -26,7 +26,7 @@ class TestFunWithFiles < FunWith::Files::TestCase
       assert_respond_to( FunWith::Files, :root )
       assert_respond_to( FunWith::Files, :version )
       
-      assert_equal "0.0.11", FunWith::Files.version   # Gotta change with every point release.  Ick.
+      assert_equal "0.0.13", FunWith::Files.version   # Gotta change with every point release.  Ick.
     end
   end
 end
